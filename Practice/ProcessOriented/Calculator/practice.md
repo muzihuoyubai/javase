e2e5efd920e1f8f079358c3acf009227f75f84df
李熠 <liyi@banyuan.club>
2020-01-17 15:24:53 +0800

    优化内容

--- Practice/ProcessOriented/Calculator/practice.md
+++ Practice/ProcessOriented/Calculator/practice.md
@@ -6,5 +6,7 @@
 2. 除法操作的时候，没有对被除数是否为0进行判断，增加此判断，如果输入为0给与提示（输入错误，不能使用0作为除数！），需要重新输入
 
 ## 练习提交路径
-如果在编写代码中遇到了错误，请将错误和解决方式以markdown格式整理下来，保存在$GIT/ProcessOriented/Calculator/practice.md
+如果在编写代码中遇到了错误，请将问题和解决方式以markdown格式整理下来，记录在本文件中，将文件提交到指定路径下$GIT/ProcessOriented/Calculator/practice.md
 将修改过的Cal.java保存在$GIT/ProcessOriented/Calculator/Cal.java
+
+## 遭遇到的问题
--- ProcessOriented/Storage/ArrayDemo.java
+++ ProcessOriented/Storage/ArrayDemo.java
@@ -6,44 +6,73 @@
 public class ArrayDemo {
 
   public static void main(String[] args) {
+    /*
+     *  数组的声明和初始化
+     */
     // 和c语言不同，并不是在声明数组的时候就分配了内存空间
-    int[] intArr1;
-    intArr1 = new int[5];
+    int[] arrInit;
+    // 使用new 创建数组
+    arrInit = new int[5];
 
     // 声明变量的时候创建数组并初始化
     // 等价于
-    // int[] intArr2 = new int[]{1, 2, 3, 4, 5};
-    int[] intArr2 = {1, 2, 3, 4, 5};
+    // int[] arrInit1 = new int[]{1, 2, 3, 4, 5};
+    int[] arrInit1 = {1, 2, 3, 4, 5};
 
     // 可以先声明，然后在创建并赋值
-    int[] intArr3;
-    intArr3 = new int[]{1, 2, 3, 4, 5};
+    int[] arrInit2;
+    arrInit2 = new int[]{1, 2, 3, 4, 5};
+
+    // 如果不赋值，则无法使用，变量必须被初始化之后才能使用
+    // int[] nullArray; // 对于下面println的时候将报错
+    int[] nullArray = null;
+    System.out.println(nullArray); // null
+    // System.out.println(nullArray.length); // java.lang.NullPointerException
+
+    /*
+     * 数组的长度
+     */
+    int[] lengthArr = new int[3];
+    // 使用length获取数组的长度。
+    // length 可以看成是一个数组的成员, 是一个final int 类型
+    // 随着new 创建数组后被初始化
+    System.out.println(lengthArr.length); // 3
 
     // 可以创建出一个数组长度为0的数组
-    int[] intEmptyArr = new int[0];
-/*
+    int[] emptyArr = new int[0];
+
+    /*
     // 不允许访问不存在的数组元素，将出现异常
     System.out.println(intArr3[5]); // 运行将报错 ArrayIndexOutOfBoundsException
-*/
+    */
+
     // 使用for each遍历数组
-    int[] intArr4 = new int[2];
-    for (int elem : intArr4) {
+    int[] foreachArr = new int[2];
+    for (int elem : foreachArr) {
       // 分别输出两个0，数组在创建的时候默认进行了初始化
       System.out.println(elem);
     }
 
     // 数组变量的相互赋值，是赋值的引用，对其中一个内容进行修改，会被另一个数组变量获取到
-    int[] intArr5 = new int[2];
-    intArr5[0] = 10;
-    intArr5[1] = 10;
-    int[] intArr6 = intArr5;
-    System.out.println(intArr5[1]); // 输出10
-    intArr6[1] = 20;
-    System.out.println(intArr5[1]); // 输出20
+    int[] refArr1 = new int[2];
+    refArr1[0] = 10;
+    refArr1[1] = 10;
+    int[] refArr2 = refArr1;
+    System.out.println(refArr1[1]); // 输出10
+    refArr2[1] = 20;
+    System.out.println(refArr1[1]); // 输出20
 
-    
-    int[] a = null;
-    System.out.println(a.length);
+    /*
+     * 数组的初始值
+     */
+    System.out.println((new int[1])[0]); // 0
+    System.out.println((new byte[1])[0]); // 0
+    System.out.println((new short[1])[0]); // 0
+    System.out.println((new char[1])[0]); // \u0000 空字符
+    System.out.println((new boolean[1])[0]); // false
+    System.out.println((new float[1])[0]); // 0.0
+    System.out.println((new double[1])[0]); // 0.0
+    System.out.println((new String[1])[0]); // null
   }
 
 }
