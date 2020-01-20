e65c64360d55ec2d87c4d0e7fe6dbd5acf84a747
李熠 <liyi@banyuan.club>
2020-01-20 16:40:55 +0800

    优化数组demo内容

--- ProcessOriented/Operation/ConversionDemo.java
+++ ProcessOriented/Operation/ConversionDemo.java
@@ -20,7 +20,7 @@ public class ConversionDemo {
     float floatNum = intNum;
     System.out.println(floatNum); // 1.23456792E8 只能保证最多7位精度
 
-    // char、byte、short 之间的算数运算结果都是int
+    // char、byte、short 变量之间的算数运算结果都是int
     short shortOne = 1;
     short shortTwo = 1;
     // 两个short运算后结果是int数据类型，不能再赋值给short
@@ -30,16 +30,10 @@ public class ConversionDemo {
     // shortTwo += shortThree; // 可以正常运算
 
     /*
-     * 基本数据类型和字符串使用+号连接，会转变成数值对应的字符串
+     * 强制数据类型转换
      */
-    // 使用 + 号，拼接字符串
-    System.out.println(3 + 4 + "字符串"); // 7字符串
-    System.out.println("字符串" + 3 + 4); // 字符串34
-    float floatTwo = 3.14F;
-    String str = "" + floatTwo; // 和字符串拼接后值的数据类型是引用数据类型
-    System.out.println("浮点数转字符串:" + str);
-
     // 高位截断，截断32bit以上的所有位，剩下32bit
+    System.out.println(0xFFFFFFFFFFL); // 1099511627775
     System.out.println((int) 0xFFFFFFFFFFL); // -1
 
     double doubleOne = 9.9;
@@ -47,14 +41,21 @@ public class ConversionDemo {
     int intTwo = (int) doubleOne;
     System.out.println(intTwo); // 9
 
-    // 关系运算符最后返回一个boolean类型
-    System.out.println(1 == 2); // 输出 false
-    System.out.println(3 != 4); // 输出 true
-    System.out.println(5 >= 6); // 输出 false
-    System.out.println(7 <= 8); // 输出 true
+    /*
+     * 基本数据类型和字符串使用+号连接，会转变成数值对应的字符串
+     */
+    // 使用 + 号，拼接字符串
+    System.out.println("字符串：" + 15); // 字符串：15
+    System.out.println("字符串：" + 'a'); // 字符串：a
+    System.out.println("字符串：" + true); // 字符串：true
+    System.out.println("字符串：" + 3.14); // 字符串：3.14
 
-    // 逻辑运算符，参与的两个操作数是boolean类型，返回一个boolean类型
-    System.out.println(1 | 2);
+    System.out.println(3 + 4 + "字符串"); // 7字符串
+    System.out.println("字符串" + 3 + 4); // 字符串34
+
+    float floatTwo = 3.14F;
+    String str = "" + floatTwo + true; // 和字符串拼接后值的数据类型是引用数据类型
+    System.out.println("转字符串:" + str); // 转字符串:3.14true
 
   }
 }
